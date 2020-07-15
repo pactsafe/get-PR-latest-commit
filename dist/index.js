a1de8f5a44861313657379707e82dedaa0678394
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2020-07-15 17:32:37 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,23 @@ const { Octokit } = require("@octokit/core");
 
 async function run() {
     try {
-      const repository = core.getInput('repository');
       const pr_number = core.getInput('pull-number');
       const token = core.getInput('token');
-
       const octokit = new Octokit({ auth: `${token}` });
-      const response = await octokit.request('GET /repos/{repository}/pulls/{pull_number}/commits', {
-        repository: '${repository}',
+      
+      const repository = core.getInput('repository');
+      const splitRepository = repository.split('/');
+      if (splitRepository.length !== 2 ||
+          !splitRepository[0] ||
+          !splitRepository[1]) {
+          throw new Error(`Invalid repository '${repository}'. Expected format {owner}/{repo}.`);
+      }
+      const owner = splitRepository[0];
+      const repo = splitRepository[1];
+      
+      const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
+        owner: '${owner}',
+        repo: '${repo}',
         pull_number: pr_number
       });
       console.log(`${response}`);
