e4edcb629abb69cf2aefcdda19e1ee6e3c0740b2
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2020-07-31 17:19:27 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -45,7 +45,7 @@ async function run() {
       
       const context_json_path = path.join(outputPath, 'latest_commit.json');
       const fs = require('fs');
-      fs.writeFile(context_json_path, response.data[index], (err) => {
+      fs.writeFile(context_json_path, console.log(response.data[index]), (err) => {
           // In case of a error throw err. 
           if (err) throw err;
       });
